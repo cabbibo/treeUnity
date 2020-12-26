42fe9a30dfa3ae007741d29c388943bc1ad7682d
Isaac Cohen <isaaclandoncohen@gmail.com>
2020-12-26 12:22:23 -0800

    getting mesh building

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class RotateAround : MonoBehaviour
+{
+
+    public Transform look;
+
+    public float lookUp;
+    public float up;
+    public float radius;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3( Mathf.Sin(Time.time * speed)  * radius , up , -Mathf.Cos(Time.time * speed)  * radius  );
+        transform.LookAt( look.position + Vector3.up * lookUp );
+    }
+}
